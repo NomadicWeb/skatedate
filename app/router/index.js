6b70b58469ce3c655aa6b3f47e53a57db4444dbf
Luke Murphy <lukewm@riseup.net>
2014-11-08 20:30:25 -0200

    line up route code for readibility

--- app/router/index.js
+++ app/router/index.js
@@ -1,15 +1,15 @@
 module.exports = function (app) {
     app.use('/',         require('./routes/index/index.js'));
-    app.use('/wallie2', require('./routes/wall2/index.js'));
-    app.use('/wallie3', require('./routes/wall3/index.js'));
-    app.use('/logo', require('./routes/logo/index.js'));
-    app.use('/intro', require('./routes/intro/index.js'));
+    app.use('/wallie2',  require('./routes/wall2/index.js'));
+    app.use('/wallie3',  require('./routes/wall3/index.js'));
+    app.use('/logo',     require('./routes/logo/index.js'));
+    app.use('/intro',    require('./routes/intro/index.js'));
     app.use('/about-us', require('./routes/aboutUs/index.js'));
-    app.use('/why', require('./routes/why/index.js'));
-    app.use('/idea', require('./routes/idea/index.js'));
+    app.use('/why',      require('./routes/why/index.js'));
+    app.use('/idea',     require('./routes/idea/index.js'));
     app.use('/problems', require('./routes/problems/index.js'));
-    app.use('/you', require('./routes/you/index.js'));
-    app.use('/form', require('./routes/form/index.js'));
+    app.use('/you',      require('./routes/you/index.js'));
+    app.use('/form',     require('./routes/form/index.js'));
     app.use('/api',      require('./routes/api/api.js'));
     app.use('/api/user', require('./routes/api/api-user.js'));
 };
